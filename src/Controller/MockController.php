a106ec76b800cb136c6ffd0de650621382621000
Malte Schlüter <malte.schlueter@webpros.com>
2024-06-18 15:35:02 +0200

    Add interval responses

--- src/Controller/MockController.php
+++ src/Controller/MockController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Clock\ClockAwareTrait;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,6 +13,8 @@ use Symfony\Component\Routing\Attribute\Route;
 #[AsController]
 final class MockController extends AbstractController
 {
+    use ClockAwareTrait;
+
     #[Route('/status/{code}', name: 'mock_status_code_always')]
     public function always(int $code): Response
     {
@@ -22,6 +25,30 @@ final class MockController extends AbstractController
     public function randomly(int $code): Response
     {
         if ((bool) random_int(0, 1)) {
+            $code = 200;
+        }
+
+        $content = $code . ' - ' . Response::$statusTexts[$code] ?? 'Unknown status code';
+
+        return new Response(content: $content, status: $code);
+    }
+
+    #[Route('/status/{code}/interval/{interval}/{time}', name: 'mock_status_code_interval')]
+    public function interval(int $code, string $interval, int $time): Response
+    {
+        $now = $this->clock->now();
+
+        $timestamp = $now->getTimestamp();
+
+        $intervalTime = match ($interval) {
+            'second' => $time,
+            'minute' => $time * 60,
+            'hour' => $time * 60 * 60,
+            'day' => $time * 60 * 60 * 24,
+            default => throw new \RuntimeException('Invalid interval'),
+        };
+
+        if ($timestamp % ($intervalTime * 2) > $intervalTime) {
             $code = 200;
         }
 
